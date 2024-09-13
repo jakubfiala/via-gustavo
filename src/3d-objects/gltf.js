f452f11f94849c4b60f67f7901a16c57c6a6bf1b
jakubfiala <j4kub.fiala@gmail.com>
2024-09-13 11:37:48 +0200

    prod build init lol

--- src/3d-objects/gltf.js
+++
@@ -1,6 +0,0 @@
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
-
-export const loadGLTF = (path) => new Promise((resolve, reject) => {
-  const loader = new GLTFLoader().setPath(path);
-  loader.load('model.gltf', async (gltf) => resolve(gltf.scene), () => {}, reject);
-});
--- src/constants.js
+++
@@ -1,9 +0,0 @@
-export const LOCALSTORAGE_POSITION_KEY = 'vg_pos';
-export const LOCALSTORAGE_INVENTORY_KEY = 'vg_inv';
-export const LOCALSTORAGE_CHAPTERS_KEY = 'vg_cha';
-// test location
-// export const START_POSITION = { lat: -20.506808759909188, lng: -69.37564029846219 };
-// actual location
-export const START_POSITION = { lat: -20.4693001, lng: -69.4563347 };
-export const START_POV = { heading: 296.84, pitch: 3 };
-export const MAPS_API_KEY = 'AIzaSyA1VOiLnJEwz3HzcDxEExa_tCTu5KKOoqQ';
