f399ef7953c44a551042f7d324cc20cfe16c07b9
jakubfiala <j4kub.fiala@gmail.com>
2024-11-08 19:26:29 +0100

    memoize gltf promises, not results

--- src/items/3d-objects/gltf.js
+++ src/items/3d-objects/gltf.js
@@ -2,17 +2,28 @@ import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
 const memoized = new Map();
 
-export const loadGLTF = (path) => new Promise((resolve, reject) => {
-  if (memoized.has(path)) {
-    return resolve(memoized.get(path).clone(true));
-  }
-
+const load = (path) => new Promise((resolve, reject) => {
   const loader = new GLTFLoader().setPath(path);
-
   const onLoad = async ({ scene }) => {
-    memoized.set(path, scene);
     resolve(scene);
   };
 
   loader.load('model.gltf', onLoad, () => {}, reject);
 });
+
+export const loadGLTF = async (path) => {
+  if (memoized.has(path)) {
+    console.info('[3d-objects]', 'memoized GLTF for', path);
+    const scene = await memoized.get(path);
+    return scene.clone(true);
+  }
+
+  // memoize the promise, so that in case multiple objects
+  // with the same GLTF load at once, they all wait for the single
+  // request to fullfill, rather than all sending their individual requests
+  // (because the initial GLTF hasn't been memoized yet).
+  const promise = load(path);
+  memoized.set(path, promise);
+
+  return promise;
+};
