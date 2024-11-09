b650891a3d66d20bf6cf15a6859b233d7ec064c1
jakubfiala <j4kub.fiala@gmail.com>
2024-11-09 12:12:21 +0100

    voice of the dead man embed

--- src/items/descs.js
+++ src/items/descs.js
@@ -429,6 +429,14 @@ export default [
     },
   },
   {
+    name: 'La Voz del Hombre Muerto',
+    collectible: false,
+    position: { lat: -20.20467, lng: -69.798 },
+    create(makers) {
+      return makers.embed('https://www.youtube.com/embed/videoseries?si=BF5VloQj6h6x37CR&amp;list=PL_KxaQOvvGcCrqyd5bWfnUEqnkVJVjFhQ');
+    },
+  },
+  {
     name: 'Humberstone Wikipedia',
     collectible: false,
     position: { lat: -20.20635, lng: -69.79717 },
