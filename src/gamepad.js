3aa4a47c513db9c984d0784a8b8471d6413084fb
jakubfiala <j4kub.fiala@gmail.com>
2025-11-06 15:10:37 +0100

    better gamepad movement

--- src/gamepad.js
+++ src/gamepad.js
@@ -1,12 +1,15 @@
-import { clamp, deg } from "./utils";
+import { clamp, deg, rad } from "./utils";
+import { throttle } from "lodash";
 
 const MIN_MOVEMENT = 0.2;
 const OFFSET_X =  0; // 0.051000000000000004;
 const OFFSET_Y =  0; // -0.045;
-const ACCEL_VELOCITY = 0.00001;
+const ACCEL_VELOCITY = 5e-6;
 const TURN_VELOCITY = 2.0;
 
 const roundFloat = (x, y) => Math.round (x / y) * y;
+
+let gamepadMove = null;
 
 export default (scriptContext) => {
   window.addEventListener("gamepadconnected", function(e) {
@@ -14,13 +17,19 @@ export default (scriptContext) => {
                 navigator.getGamepads()[0].index, navigator.getGamepads()[0].id,
                 navigator.getGamepads()[0].buttons.length, navigator.getGamepads()[0].axes.length);
 
-    const controlLoop = () => {
-      const gamepad = navigator.getGamepads()[0];
-      if (gamepad?.axes.length < 4) {
-        console.warn('[gamepad]', 'not enough joystick axes found');
-        return;
-      }
+    if (navigator.getGamepads()[0]?.axes.length < 4) {
+      console.warn('[gamepad]', 'not enough joystick axes found');
+      return;
+    }
 
+    const throttledPositionUpdate = throttle((map, newPosition) => {
+      map.setPosition(newPosition);
+    }, 500, { leading: true, trailing: true });
+
+    const controlLoop = () => {
+      if (!document.hasFocus || document.visibilityState !== "visible") return;
+
+      const gamepad = navigator.getGamepads()[0];
       const [lx, ly, rx, ry] = gamepad.axes;
 
       const rr = Math.sqrt(rx ** 2 + ry ** 2);
@@ -34,14 +43,16 @@ export default (scriptContext) => {
 
       if (Math.abs(ly) > 0.1) {
         const heading = scriptContext.map.getPov().heading;
-        const position = scriptContext.map.getPosition();
+        const position = { lat: scriptContext.map.getPosition().lat(), lng: scriptContext.map.getPosition().lng() };
 
-        const newPosition = {
-          lat: position.lat() - ACCEL_VELOCITY * ly * Math.cos(heading/180*Math.PI),
-          lng: position.lng() - ACCEL_VELOCITY * ly * Math.sin(heading/180*Math.PI),
+        gamepadMove = !gamepadMove ? position : {
+          lat: gamepadMove.lat - ACCEL_VELOCITY * ly * Math.cos(rad(heading)),
+          lng: gamepadMove.lng - ACCEL_VELOCITY * ly * Math.sin(rad(heading)),
         };
 
-        scriptContext.map.setPosition(newPosition);
+        throttledPositionUpdate(scriptContext.map, gamepadMove);
+      } else {
+        gamepadMove = null;
       }
 
       window.requestAnimationFrame(controlLoop);
